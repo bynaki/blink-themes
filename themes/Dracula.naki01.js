92acdda31f764758614da8b285854f418c012cfd
naki <bynaki@icloud.com>
2021-01-27 21:13:24 +0900

    Dracula.naki01

---
+++ themes/Dracula.naki01.js
@@ -0,0 +1,26 @@
+t.prefs_.set(
+  'color-palette-overrides',
+    [
+      "#282a36",
+      "#ff5555",
+      "#50fa7b",
+      "#f1fa8c",
+      "#bd93f9",
+      "#ff79c6",
+      "#8be9fd",
+      "#f8f8f2",
+      "#424450",
+      "#343746",
+      "#21222C",
+      "#191A21",
+      "#6272A4",
+      "#44475A",
+      "#242630",
+      "#FFB86C",
+    ]
+);
+
+t.prefs_.set('foreground-color', "#f8f8f2");
+t.prefs_.set('background-color', "#282a36");
+t.prefs_.set('cursor-color', 'rgba(205, 0, 86, 0.6)');
+t.prefs_.set('cursor-blink', false);
